58120144dff72e87c9cb3171b8cc73db0f5f27d7
slg <970398591@qq.com>
2022-05-09 17:19:54 +0800

    消融shader

--- assets/scripts/ShaderTest.ts
+++ assets/scripts/ShaderTest.ts
@@ -1,10 +1,30 @@
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export class ShaderTest extends cc.Component {
 
+    @property({ type: cc.Sprite })
+    private plane: cc.Sprite = null!;
 
-    start () {
+    private isEffect: boolean = false;
+    private dissolveThreshold: number = 0;
 
+    onBtnShader() {
+        this.isEffect = true;
+        this.dissolveThreshold = 0;
+        let mt = this.plane.getMaterial(0);
+        mt.setProperty("dissolveThreshold", this.dissolveThreshold);
+    }
+
+    update(dt: number) {
+        if (this.isEffect) {
+            this.dissolveThreshold += dt / 2;
+            if (this.dissolveThreshold >= 1) this.dissolveThreshold = 1;
+            let mt = this.plane.getMaterial(0);
+            mt.setProperty("dissolveThreshold", this.dissolveThreshold);
+            if (this.dissolveThreshold >= 1) {
+                this.isEffect = false;
+            }
+        }
     }
 }
