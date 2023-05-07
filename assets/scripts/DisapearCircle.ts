6accc76c129b43919319e3a93a613c6a7e3da7c2
WarOfCode <970398591@qq.com>
2023-05-07 14:31:32 +0800

    圆形倒计时shader

---
+++ assets/scripts/DisapearCircle.ts
@@ -0,0 +1,28 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export class DisapearCircle extends cc.Component {
+
+    @property({
+        type:cc.Sprite
+    })
+    private sp:cc.Sprite = null;
+
+    private _time:number = 3;
+
+    start () {
+
+    }
+
+    update (dt) {
+        this._time -= dt;
+        if(this._time < 0){
+            this._time = 3;
+        }
+        let percent = this._time/3;
+        if(percent > 1) percent = 1;
+        if(percent < 0) percent = 0;
+        let m = this.sp.getMaterial(0);
+        m.setProperty("disapearValue", percent);
+    }
+}
